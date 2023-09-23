91ffc8f4f34f0a30720bc2fadd12190a0b209c70
SpartG <locussttt@gmail.com>
2023-09-23 16:01:03 +0200

    add dialogue ui for interaction moine

--- gameobject/moine/moine.gd
+++ gameobject/moine/moine.gd
@@ -7,6 +7,8 @@ var node_name : String = "Moine"
 @export var sprite : Sprite2D
 
 var player_looking : bool
+var _dialogue_box := preload("res://UI/Components/dialogue_box.tscn")
+
 
 func interact_available(state : bool) -> void:
 	player_looking = state
@@ -19,4 +21,5 @@ func _ready() -> void:
 
 func _unhandled_key_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("interact") and player_looking:
+		get_tree().root.add_child(_dialogue_box.instantiate())
 		print("speak")
