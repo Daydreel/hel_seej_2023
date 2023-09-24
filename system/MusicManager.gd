d7950a6f39615a2611c9b8a304942917abb336bc
SpartG <locussttt@gmail.com>
2023-09-24 16:15:40 +0200

    fix : sound transition

--- system/MusicManager.gd
+++ system/MusicManager.gd
@@ -14,16 +14,22 @@ func music_transition(music_selected : int) -> void:
 	if back:
 		self.stream = music[music_selected]
 		self.play()
-		tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_LINEAR)
-		tween.tween_property(self, "volume_db", linear_to_db(volume), 2.0)
-		tween.tween_property(audio_stream_2, "volume_db", linear_to_db(0.0), 2.0)
+		
+		transition_player.play_backwards("transition")
+#		tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_LINEAR)
+#		tween.tween_property(self, "volume_db", linear_to_db(volume), 2.0)
+#		tween.tween_property(audio_stream_2, "volume_db", linear_to_db(0.0), 2.0)
+		
+		
 		back = false
 	else:
 		audio_stream_2.stream = music[music_selected]
 		audio_stream_2.play()
-		tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_LINEAR)
-		tween.tween_property(audio_stream_2, "volume_db", linear_to_db(volume), 2.0)
-		tween.tween_property(self, "volume_db", linear_to_db(0.0), 2.0)
+		
+		transition_player.play("transition")
+#		tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_LINEAR)
+#		tween.tween_property(audio_stream_2, "volume_db", linear_to_db(volume), 2.0)
+#		tween.tween_property(self, "volume_db", linear_to_db(0.0), 2.0)
 		back = true
 
 func change_volume(power : float) -> void:
