e7dadd6438b2083cfc8d7806c371c21687066ec0
SpartG <locussttt@gmail.com>
2023-09-24 15:54:01 +0200

    add : sfx monster + wind (end screen)

--- UI/Screens/end_screen.gd
+++ UI/Screens/end_screen.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var anim_p = $AnimationPlayer
 @onready var the_end_screen = $TheEnd
 @onready var button = $Button
+@onready var wind_sfx = $AudioStreamPlayer
 
 func _ready():
 	anim_p.play("scary")
@@ -15,6 +16,7 @@ func _on_animation_player_animation_finished(anim_name):
 	button.set_visible(true)
 	the_end_screen.set_visible(true)
 	MusicManager.music_transition(5)
+	wind_sfx.stop()
 
 
 func _on_button_pressed() -> void:
