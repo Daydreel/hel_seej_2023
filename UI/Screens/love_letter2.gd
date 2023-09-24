d22a1679793f03188267319c1b0857edd966f4c7
SpartG <locussttt@gmail.com>
2023-09-24 16:33:51 +0200

    add : intros scenes + tweak : foot step sfx

--- UI/Screens/love_letter2.gd
+++ UI/Screens/love_letter2.gd
@@ -2,7 +2,5 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	SceneTransition.goto_scene(SceneTransition.scenes[6].resource_path)
+	SceneTransition.goto_scene(SceneTransition.scenes[7].resource_path)
 #	MusicManager.music_transition(1)
-	
-	
