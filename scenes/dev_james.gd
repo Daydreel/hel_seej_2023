5201deaa0e8826a802aec59d0e42210e625a63bb
JK <daydreel@gmail.com>
2023-09-27 15:57:49 +0200

    Deleted test scenes

--- scenes/dev_james.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-
-func _ready():
-	var dialogue = load("res://UI/Components/dialogue_box.tscn").instantiate()
-	dialogue.dialogue_path = "res://assets/dialogues/d1_test.dialogue"
-	add_child(dialogue)
