71a9e0bc22111fb84f869891d9292bde670dcef3
JK <daydreel@gmail.com>
2023-09-27 15:30:45 +0200

    No game over

--- gameobject/moine/moine.gd
+++ gameobject/moine/moine.gd
@@ -1,10 +1,7 @@
 extends CharacterBody2D
 class_name Moine
 
-signal target_interact
-
 var has_talked = false
-var node_name : String = "Moine"
 
 @onready var sprite : Sprite2D = $Sprite2D
 
@@ -16,14 +13,16 @@ var _dialogue_box := preload("res://UI/Components/dialogue_box.tscn")
 @export var music_state : int
 @export var music_change : bool
 
+func create_dialogue(path : String, image : Texture) -> void:
+		var dialogue_box : DialogueBox = _dialogue_box.instantiate()
+		dialogue_box.dialogue_path = path
+		dialogue_box.monk_texture = image
+		get_tree().root.add_child(dialogue_box)
 
 func _on_aggro_range_area_entered(area):
 	if !has_talked :
 		has_talked = true
-		var dialogue_box : DialogueBox = _dialogue_box.instantiate()
-		dialogue_box.dialogue_path = dialogue_path
-		dialogue_box.monk_texture = monk_texture
-		get_tree().root.add_child(dialogue_box)
-
+		create_dialogue(dialogue_path, monk_texture)
+		
 		if music_change:
 			MusicManager.music_transition(music_state)
